1abd26ebd53b553b9fcda67712a8931eb80d5a64
Devkot07 <devkot07@gmail.com>
2024-08-20 15:42:49 +0300

    done

---
+++ app/src/test/java/com/example/marsphotos/fake/NetworkMarsRepositoryTest.kt
@@ -0,0 +1,25 @@
+package com.example.marsphotos.fake
+
+import com.example.marsphotos.data.NetworkMarsPhotoRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NetworkMarsRepositoryTest {
+
+    @Test
+    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
+        runTest {
+            val repository = NetworkMarsPhotoRepository(
+                marsApiService = FakeMarsApiService()
+            )
+            assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
+        }
+
+
+    @Test
+    fun networkMarsRepository_(){
+
+    }
+
+}
